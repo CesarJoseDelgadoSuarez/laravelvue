c22064216a4a1ac8974fd16ffefaf3094c0db1cd
Cesar Delgado <cjdsgithub@gmail.com>
2023-07-30 22:35:09 +0100

    actualizada rutas vue

--- resources/js/routes/index.js
+++ resources/js/routes/index.js
@@ -1,25 +1,30 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import HomeComponent from '../vue/views/Home.vue'
-import DashboardComponent from '../vue/views/Dashboard.vue'
-
+import HomeComponent from "../vue/views/Home.vue";
+import DashboardComponent from "../vue/views/Dashboard.vue";
+import ItemsListComponent from "../vue/views/itemList.vue";
 
 const routes = [
     {
-        path:'/',
-        name:'home',
-        component: HomeComponent
+        path: "/",
+        name: "home",
+        component: HomeComponent,
     },
     {
-        path:'/dashboard',
-        name:'',
-        component: DashboardComponent
+        path: "/dashboard",
+        name: "",
+        component: DashboardComponent,
     },
-]
+    {
+        path: "/items",
+        name: "",
+        component: ItemsListComponent,
+    },
+];
 
 const router = createRouter({
     history: createWebHistory(),
-    routes
-})
+    routes,
+});
 
-export default router
+export default router;
