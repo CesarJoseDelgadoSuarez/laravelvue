2814a0b33388cb7a9aa4e4c4f25df5905a518cd7
Cesar Delgado <cjdsgithub@gmail.com>
2023-07-30 22:34:55 +0100

    Añadido api de items para la base de datos

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Item;
 class ItemController extends Controller
 {
     /**
@@ -11,7 +11,7 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+        return Item::orderBy('name', 'DESC')->get();
     }
 
     /**
@@ -27,7 +27,21 @@ class ItemController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $newItem = new Item;
+
+        $newItem->name = $request->item["name"];
+        $newItem->description = $request->item["description"];
+        $newItem->stock_min = $request->item["stock_min"];
+        $newItem->actual_stock = $request->item["actual_stock"];
+        $newItem->ubication = $request->item["ubication"];
+        $newItem->supplier = $request->item["supplier"];
+        $newItem->price = $request->item["price"];
+        $newItem->benefit = $request->item["benefit"];
+
+        $newItem->save();
+
+        return $newItem;
+
     }
 
     /**
@@ -49,9 +63,27 @@ class ItemController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $existingItem = Item::find( $id );
+
+        if($existingItem){
+
+            $existingItem->name = $request->item["name"] ? $request->item["name"] : $existingItem->name;
+            $existingItem->description = $request->item["description"] ? $request->item["description"] : $existingItem->description;
+            $existingItem->stock_min = $request->item["stock_min"] ? $request->item["stock_min"] : $existingItem->stock_min;
+            $existingItem->actual_stock = $request->item["actual_stock"] ? $request->item["actual_stock"] : $existingItem->actual_stock;
+            $existingItem->ubication = $request->item["ubication"] ? $request->item["ubication"] : $existingItem->ubication;
+            $existingItem->supplier = $request->item["supplier"] ? $request->item["supplier"] : $existingItem->supplier;
+            $existingItem->price = $request->item["price"] ? $request->item["price"] : $existingItem->price;
+            $existingItem->benefit = $request->item["benefit"] ? $request->item["benefit"] : $existingItem->benefit;
+
+            $existingItem->save();
+
+            return $existingItem;
+        }
+
+        return "Item Not Found";
     }
 
     /**
@@ -59,6 +91,13 @@ class ItemController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $existingItem = Item::find( $id );
+
+        if ($existingItem) {
+            $existingItem->delete();
+            return "Item Succesfully Deleted";
+        }
+
+        return "no item found with the given id";
     }
 }
